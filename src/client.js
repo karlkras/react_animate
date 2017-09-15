69a303b994095e21b7e6d074a8e10951bc20c2fc
kkras2 <karl.krasnowsky@nike.com>
2017-09-14 18:55:21 -0700

    amimation stuff.

--- src/client.js
+++ src/client.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './appsource.scss';
 import './bootstrap/button.scss';
 
 
 class ClientComponent extends Component {
+
+    constructor(params) {
+        super(params);
+
+        this.onClick = this.onClick.bind(this);
+
+        this.state = { click: 0};
+    }
+
+
+    onClick(event) {
+        this.setState({click: this.state.click + 1});
+    }
+
+
     render() {
         return(
-            <Jumbotron 
-                className="-fluid text-center custom"
-                containerFluid={true}
+            <ReactCSSTransitionGroup
+                transitionName='app'
+                transitionAppear={true}
+                transitionEnterTimeout={500}
+                transitionLeaveTimeout={500}
+                transitionAppearTimeout={500}
             >
-                <h1>
-                    Building react.js User Interfaces
-                </h1>
-                <p>with Bootstrap and SASS</p>
-                <p>
-                    <Button className='-primary' href="//02geek.com" target="_blank" disabled="true" buttonText="This is a link, dammit!"/>
-                    <Button className='-danger' buttonText="This is just a button!"/>
-                </p>
-            </Jumbotron>
+                <Jumbotron
+                    className="-fluid text-center custom"
+                    containerFluid={true}
+                >
+                    <h1>
+                        Building react.js User Interfaces
+                    </h1>
+                    <p>with Bootstrap and SASS</p>
+                    <p>
+                        <Button
+                            className='-primary'
+                            href="//02geek.com"
+                            target="_blank"
+                            buttonText="This is a link, dammit!"
+                        />
+                        <Button
+                            onClick={this.onClick}
+                            className='-danger'
+                            buttonText={this.state.click}
+                        />
+                    </p>
+                </Jumbotron>
+            </ReactCSSTransitionGroup>
         );
     }
 }
